783d984d91b973d77ad99ebcfffdf49dd2a20d50
Andrea Manzini <ilmanzo@gmail.com>
2025-06-17 11:18:33 +0200

    make build script export PIN_ROOT env var

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
 # download and extract intel pin
 pushd ..
-test -d pin* || curl https://software.intel.com/sites/landingpage/pintool/downloads/pin-external-3.31-98869-gfa6f126a8-gcc-linux.tar.gz | tar zxf -
-export PIN_ROOT=$(realpath `ls -d pin*`)
+curl https://software.intel.com/sites/landingpage/pintool/downloads/pin-external-3.31-98869-gfa6f126a8-gcc-linux.tar.gz | tar zxf -
+PIN_ROOT=$(realpath `ls -d pin*`)
 popd
 # builds the custom library (pintool)
 make
+echo "export PIN_ROOT=$PIN_ROOT" > env
 # build code coverage target example
 #pushd example && make && popd
 # run code coverage tool
