5342bd0a505ea6bfeff683b94d74d4bb1923038e
Andrea Manzini <ilmanzo@gmail.com>
2025-06-17 11:08:01 +0200

    prepare for release

--- build.sh
+++ build.sh
@@ -3,16 +3,11 @@ pushd ..
 test -d pin* || curl https://software.intel.com/sites/landingpage/pintool/downloads/pin-external-3.31-98869-gfa6f126a8-gcc-linux.tar.gz | tar zxf -
 export PIN_ROOT=$(realpath `ls -d pin*`)
 popd
-
 # builds the custom library (pintool)
-
 make
-
 # build code coverage target example
-pushd example && make && popd
-
+#pushd example && make && popd
 # run code coverage tool
-
 #$PIN_ROOT/pin -t ./obj-intel64/FuncTracer.so -- example/cov_sample 7 3
 
 
