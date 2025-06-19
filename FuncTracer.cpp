32d54279c6aa82acfb8150b785cd6d992a28be51
Andrea Manzini <ilmanzo@gmail.com>
2025-06-19 08:02:42 +0200

    c++ 20

--- FuncTracer.cpp
+++ FuncTracer.cpp
@@ -22,11 +22,8 @@ VOID log_function_call(const char *img_name, const char *func_name)
 
 bool isBlacklisted(const std::string &func_name)
 {
-    // Check if the function name ends with '@plt'
-    if (func_name.size() >= 4 && func_name.compare(func_name.size() - 4, 4, "@plt") == 0) return true; // Skip PLT entries
-    // Check if the function name starts with '__'
-    // if (func_name.size() >= 2 && func_name.compare(0, 2, "__") == 0) return true; // Skip internal functions
-
+    if func_name.ends_with("@plt") return true; // Skip PLT entries
+    // if func_name.begins_with("__") return true; // Skip internal functions
     // Check if the function name is in the blacklist
     static const std::set<std::string> blacklist = {
         "main", "_init", "_start"
