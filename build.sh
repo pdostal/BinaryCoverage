50e76061749c6a79be836ff58f365a771c7e13ea
Andrea Manzini <ilmanzo@gmail.com>
2025-06-19 08:10:00 +0200

    make build script more robust

--- build.sh
+++ build.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 # download and extract intel pin
+PIN_ARCHIVE="pin-external-3.31-98869-gfa6f126a8-gcc-linux"
+PIN_URL="https://software.intel.com/sites/landingpage/pintool/downloads/${PIN_ARCHIVE}.tar.gz"
+
 pushd ..
-curl https://software.intel.com/sites/landingpage/pintool/downloads/pin-external-3.31-98869-gfa6f126a8-gcc-linux.tar.gz | tar zxf -
+
+if [ ! -d "$PIN_ARCHIVE" ]; then
+    curl "$PIN_URL" | tar zxf -
+fi
+if [ ! -d "$PIN_ARCHIVE" ]; then
+    echo "Error: Failed to download or extract Intel Pin archive." >&2
+    exit 1
+fi
+
 export PIN_ROOT=$(realpath `ls -d pin*`)
 popd
 # builds the custom library (pintool)
